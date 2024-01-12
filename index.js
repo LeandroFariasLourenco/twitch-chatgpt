39fb851d73e96a75d71f6a35eeeeb50f069353a8
Leandro <leandro.farias01@outlook.com>
2024-01-11 21:13:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ app.get('/gpt/:text', async (req, res) => {
         console.log("User Input: " + text)
 
         const response = await openai.createCompletion({
-            model: "text-davinci-003",
+            model: "gpt-3.5-turbo",
             prompt: prompt,
             temperature: 0.7,
             max_tokens: 256,
